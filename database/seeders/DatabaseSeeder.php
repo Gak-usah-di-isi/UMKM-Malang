1552724e5be9814634a0ad141d6eb3424e545403
Anas Khalif Muttaqien <anaskhalif995@gmail.com>
2025-08-15 20:03:01 +0700

    fix : fixing article category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             LaratrustSeeder::class,
             UmkmCategorySeeder::class,
             EventSeeder::class,
+            ArticleCategorySeeder::class,
         ]);
     }
 }
